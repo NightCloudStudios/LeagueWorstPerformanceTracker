800e07d3f5ebae8101bb19edc2bc32a58265e87f
Jacob <104500838+JustExisting1@users.noreply.github.com>
2025-01-29 23:10:53 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/LeagueWorstPerformanceTracker/",
+  basePath: "/LeagueWorstPerformanceTracker",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
