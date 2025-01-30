44159fc9055ee3d9e0f2d830f832633f6f736ba0
Jacob <104500838+JustExisting1@users.noreply.github.com>
2025-01-30 02:25:33 +0000

    Created Branch

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/LeagueWorstPerformanceTracker",
+  // basePath: "/LeagueWorstPerformanceTracker", FOR GIT PAGES ONLY
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
