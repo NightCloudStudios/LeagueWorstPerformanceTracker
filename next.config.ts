0aaf454edebdf5fc4b8d7a773858f1139680bb1d
Jacob <104500838+JustExisting1@users.noreply.github.com>
2025-02-06 21:35:04 +0000

    changed output

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   // basePath: "/LeagueWorstPerformanceTracker", FOR GIT PAGES ONLY
   // output: "export",  // <=== enables static exports
+  output: "standalone",  // <=== enables static exports
   reactStrictMode: true,
   env: {
     RIOT_KEY: process.env.RIOT_KEY,
